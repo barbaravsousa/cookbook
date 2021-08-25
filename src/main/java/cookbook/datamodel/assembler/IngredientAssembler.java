719784e3fbb8af991a049fab7d2d4985e7c6b8c4
Bárbara Sousa <barbaravieirasousa@gmail.com>
2021-08-25 14:34:50 +0100

    Add ingredient assembler

--- src/main/java/cookbook/datamodel/assembler/IngredientAssembler.java
+++ src/main/java/cookbook/datamodel/assembler/IngredientAssembler.java
@@ -5,6 +5,8 @@ import cookbook.datamodel.IngredientJPA;
 import cookbook.datamodel.RecipeJPA;
 import cookbook.domain.Ingredient;
 import cookbook.domain.MeasureUnity;
+import cookbook.exception.InvalidNameException;
+import cookbook.util.Utils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -15,9 +17,11 @@ import java.util.List;
 public class IngredientAssembler {
 
     /**
-     * @param ingredient
-     * @param recipeJPA
-     * @return
+     * Method that receives a domain object and transforms it into a data model object.
+     *
+     * @param ingredient, domain object
+     * @param recipeJPA,  data model object
+     * @return an IngredientJPA object
      */
     public static IngredientJPA toData(Ingredient ingredient, RecipeJPA recipeJPA) {
         String name = ingredient.getName();
@@ -27,26 +31,28 @@ public class IngredientAssembler {
     }
 
     /**
+     * Method that transforms a data model object into a domain object.
      *
-     * @param ingredientJPA
-     * @return
+     * @param ingredientJPA, data model object
+     * @return an Ingredient, domain object
      */
-    public static Ingredient toDomain(IngredientJPA ingredientJPA) {
+    public static Ingredient toDomain(IngredientJPA ingredientJPA) throws InvalidNameException {
         String name = ingredientJPA.getName();
         String unit = ingredientJPA.getMeasureUnit();
-
-
+        MeasureUnity measureUnity = Utils.splitMeasureUnity(unit);
+        return new Ingredient(name, measureUnity);
     }
 
 
     /**
+     * Method that receives a list with data model objects and transforms them into domain objects and adds them to a list.
      *
-     * @param ingredientJPAList
-     * @return
+     * @param ingredientJPAList, list with data model objects
+     * @return a list with domain objects
      */
-    public static List<Ingredient> toDomain(List<IngredientJPA> ingredientJPAList) {
-        List<Ingredient>  ingredientList = new ArrayList<>();
-        for(IngredientJPA ingredientJPA:ingredientJPAList){
+    public static List<Ingredient> toDomain(List<IngredientJPA> ingredientJPAList) throws InvalidNameException {
+        List<Ingredient> ingredientList = new ArrayList<>();
+        for (IngredientJPA ingredientJPA : ingredientJPAList) {
             ingredientList.add(toDomain(ingredientJPA));
         }
         return ingredientList;
